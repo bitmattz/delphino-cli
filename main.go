83707e3c4f07bdd655b61c1117d6f71e8e581c13
bitmattz <bitencourt.a.matheus@gmail.com>
2025-04-29 01:02:45 -0300

    Creating menu and config file

--- main.go
+++ main.go
@@ -1,32 +1,59 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"os/exec"
-	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter the name of the package to install: ")
-	packageName, _ := reader.ReadString('\n')
-	packageName = strings.TrimSpace(packageName)
 
-	if packageName == "" {
-		fmt.Println("Package name cannot be empty.")
-		return
-	}
+	// CREATE MENU
+	fmt.Println(`
+$$$$$$$\  $$$$$$$$\ $$\       $$$$$$$\  $$\   $$\ $$$$$$\ $$\   $$\  $$$$$$\  
+$$  __$$\ $$  _____|$$ |      $$  __$$\ $$ |  $$ |\_$$  _|$$$\  $$ |$$  __$$\ 
+$$ |  $$ |$$ |      $$ |      $$ |  $$ |$$ |  $$ |  $$ |  $$$$\ $$ |$$ /  $$ |
+$$ |  $$ |$$$$$\    $$ |      $$$$$$$  |$$$$$$$$ |  $$ |  $$ $$\$$ |$$ |  $$ |
+$$ |  $$ |$$  __|   $$ |      $$  ____/ $$  __$$ |  $$ |  $$ \$$$$ |$$ |  $$ |
+$$ |  $$ |$$ |      $$ |      $$ |      $$ |  $$ |  $$ |  $$ |\$$$ |$$ |  $$ |
+$$$$$$$  |$$$$$$$$\ $$$$$$$$\ $$ |      $$ |  $$ |$$$$$$\ $$ | \$$ | $$$$$$  |
+\_______/ \________|\________|\__|      \__|  \__|\______|\__|  \__| \______/ 
+																			  
+`)
+	fmt.Println("Welcome to the Package Manager!")
+	fmt.Println("This program allows you to install preset packages and create folders on your system.")
+	fmt.Println("\n")
+	fmt.Println("Please choose an option:")
+	fmt.Println("1. Install a package")
+	fmt.Println("2. Create a folder")
+	fmt.Println("3. Uninstall preset packages")
+	fmt.Println("4. Delete folders")
+	fmt.Println("5. Exit")
 
-	cmd := exec.Command("sudo", "apt-get", "install", "-y", packageName)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	// READ JSON
 
-	fmt.Println("Installing package:", packageName)
-	if err := cmd.Run(); err != nil {
-		fmt.Println("Error installing package:", err)
-	} else {
-		fmt.Println("Package installed successfully.")
-	}
+	// SEGMENTATION
+
+	// INSTALATION
+
+	//UNINSTALLATION
+
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter the name of the package to install: ")
+	// packageName, _ := reader.ReadString('\n')
+	// packageName = strings.TrimSpace(packageName)
+
+	// if packageName == "" {
+	// 	fmt.Println("Package name cannot be empty.")
+	// 	return
+	// }
+
+	// cmd := exec.Command("sudo", "apt-get", "install", "-y", packageName)
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+
+	// fmt.Println("Installing package:", packageName)
+	// if err := cmd.Run(); err != nil {
+	// 	fmt.Println("Error installing package:", err)
+	// } else {
+	// 	fmt.Println("Package installed successfully.")
+	// }
 }
--- todo.md
+++ todo.md
@@ -1,5 +1,7 @@
+# TODO List
 
-- Create file for insert packages, programs and folders to create.
-
-
+2. Allow the user to populate the file with packages and folders to be created.
+3. Implement functionality to separate the packages and folders into two arrays.
+4. Develop logic to create the folders in the specified path.
+5. Create a function to undo the installation of the packages and delete the folders. (Testing purpose only)
 
